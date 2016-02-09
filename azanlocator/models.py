0435fefaf710dbbf3ca1938a84592794a19ea51d
Raden Muaz <raden.m.muaz@gmail.com>
2016-02-09 17:17:58 +0800

    next try

--- azanlocator/models.py
+++ azanlocator/models.py
@@ -10,3 +10,33 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class DailyTimes(model.Models):
+    subuh   = models.DateTimeField()
+    zuhur   = models.DateTimeField()
+    asar    = models.DateTimeField()
+    maghrib = models.DateTimeField()
+    isha    = models.DateTimeField()
+
+    currentDate = models.DateField()
+    zone = models.ForeignKey('Zone',
+                             on_delete=models.CASCADE,)
+
+class MonthlyTimes(model.Models):
+
+    zone = models.ForeignKey('Zone',
+                             on_delete=models.CASCADE,)
+
+
+
+class Zone(models.Model):
+    zoneName   = models.CharField(max_length=20)
+    stateName  = models.CharField(max_length=20)
+    ipAddress  = models.CharField(max_length=20)
+
+    todayTimes = models.ForeignKey(DailyTimes,
+                             on_delete=models.CASCADE,)
+    tomorrowTimes = models.ForeignKey(DailyTimes,
+                             on_delete=models.CASCADE,)
+
+    montlyTimes =
