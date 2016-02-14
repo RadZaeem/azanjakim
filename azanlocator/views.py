3051d42629de2eecd74484fc58d5d78ffa83edc8
Raden Muaz <raden.m.muaz@gmail.com>
2016-02-14 22:38:28 +0800

    Used HTML5 geolocation instead

--- azanlocator/views.py
+++ azanlocator/views.py
@@ -34,23 +34,25 @@ class IndexView(generic.TemplateView):
 '''
 def index(request):
     template_name = 'azanlocator/index.html'
-    new_parse = ParsedTimes()
-    new_parse.save()
+
     #ip = get_ip(request)
     #if ip == "127.0.0.1" or ip is None: ip = ""
     #print ip
     if request.GET.get('lat') and request.GET.get('lon'):
+        new_parse = ParsedTimes()
+        new_parse.save()
         #origin = Point(float(request.GET.get('lon')), float(request.GET.get('lat')))
         lat = float(request.GET.get('lat'))
         lng = float(request.GET.get('lon'))
         print (lat,lng)
         new_parse.update_times_by_db(lat,lng)
+        return render(request, template_name, {'new_parse':new_parse})
         #camps = Campground.objects.all().distance(origin).order_by('distance')
     else:
-        pass
+        return render(request, template_name)
         #camps = Campground.objects.all().order_by('name')
 
-    return render(request, template_name, {'new_parse':new_parse})
+    #return render(request, template_name, {'new_parse':new_parse})
         #print self.new_parse.zone.ip_address
 
 def detail(request, question_id):
