96bc536c28999966706441c9a24f6bb3df9f8490
RadZaeem <raden.m.muaz@gmail.com>
2017-08-24 05:55:33 +0800

    m

--- azanlocator/urls.py
+++ azanlocator/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
-
+from django.contrib.staticfiles.views import serve
 from django.views.generic import TemplateView, RedirectView
 
 
